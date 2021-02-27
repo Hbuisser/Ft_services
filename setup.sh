1c036eab05fc4248907e9f2a6480002cfe44c86d
Henry Buisseret <henrybuisseret@gmail.com>
2021-02-27 17:08:40 +0100

    nginx begin

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 function start_minikube() {
     minikube delete --all
-    # Create the Namespace (VM) that wrap everything => Minikube for creating it localy
+    # Create the Namespace (VM) that wrap everything 
+    # => Minikube for creating it localy
     minikube start --vm-driver=virtualbox
     minikube status
 
@@ -12,6 +13,8 @@ function start_minikube() {
     #minikube addons enable logviewer
 
     # Start the docker environment on the Minikube VM
+    # Minikube creates a specific VM in VirtualBox that will run your Docker images. 
+    # You need to link your shell with the Minikube context
     eval $(minikube docker-env)
 }
 
@@ -34,10 +37,12 @@ function metalib_config() {
     then
         kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
     fi
+    # https://metallb.universe.tf/configuration/
     kubectl apply -f srcs/metalib/config.yaml > /dev/null
 }
 
 function check_minikube() {
+    # https://minikube.sigs.k8s.io/docs/start/
     minikube version
 	if [ "$?" != 0 ]
 	then
@@ -50,14 +55,49 @@ function check_minikube() {
 	fi
 }
 
+function check_brew() {
+	brew -v
+	# $? is the exit status of the last executed command / true returns 0, false 1
+	if [ "$?" != 0 ]
+	then 
+		printf "Brew isn't installed!\n"
+		read -p "Do you want to install brew? (Y/N): " REP
+		if [[ $REP = "Y" || $REP = "y" || $REP = "" ]]
+		then
+			printf "Installing brew...\n"
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		else
+			printf "Brew is required for this program\n"
+			# exit 1 means exiting with false at the end
+			exit 1
+		fi
+	else
+		printf "Brew is installed ✅\n"
+	fi
+}
+
 function main() {
-    check_minikube
-    metalib_config
-    check_docker
-    start_minikube
-    #docker build srcs/nginx -t img-nginx
-    #kubectl apply -f srcs/nginx/deployment.yaml
-    #minikube dashboard
+    #check_brew
+    #check_minikube
+    #metalib_config
+    #check_docker
+    #start_minikube
+
+    docker build srcs/nginx -t img-nginx
+    kubectl apply -f srcs/nginx/deployment.yaml
+    minikube dashboard
 }
 
 main
+
+### INFOS
+
+# NGINX 
+# Nginx is a web server that can provide web pages and execute PHP.
+# You need to create a simple Nginx server, it has to be fetchable through Ingres, 
+# which is a more advanced version of service. 
+# Port 443 is for SSL connection (https). 
+# You can create a SSL certificate with Openssl. 
+# This container needs to provide a SSH connection. 
+# SSH is used to access a computer remotly through a shell. 
+# A really simple way to create a SSH server is through the openssh package and then run the sshd daemon.
