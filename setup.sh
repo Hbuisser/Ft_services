519926f2c374b9c15a5efdf4ba6ff321e8361c8c
Henry Buisseret <henrybuisseret@gmail.com>
2021-03-03 11:05:16 +0100

    services tls connection problem

--- setup.sh
+++ setup.sh
@@ -1,88 +1,47 @@
 #! /bin/sh
 
 function start_minikube() {
-    minikube stop
-    minikube delete --all
+    #minikube stop
+    #minikube delete --all
+
     # Create the Namespace (VM) that wrap everything 
-    # => Minikube for creating it localy
     minikube start --vm-driver=virtualbox
     minikube status
 
     minikube addons enable metrics-server
     minikube addons enable dashboard
     minikube addons enable metallb
-    #minikube addons enable logviewer
+
+    minikube kubectl -- get po -A
+    # create the cluster
 
     # Start the docker environment on the Minikube VM
     # Minikube creates a specific VM in VirtualBox that will run your Docker images. 
     # You need to link your shell with the Minikube context
-    eval $(minikube docker-env)
+    #eval $(minikube docker-env)
 }
 
-function check_docker() {
-    docker -v
-	if [ "$?" != 0 ]
-	then
-		printf "Docker isn't installed!\n"
-		printf "You can download it here : https://www.thegeekdiary.com/how-to-install-docker-on-mac/ \n"
-        exit 1
-	else
-		printf "Docker is installed ✅\n"
-	fi
-}
-
-function metalib_config() {
-    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-    # if [ "$(kubectl get secrets --namespace metallb-system | grep memberlist)" = "" ]
-    # then
-    #     kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-    # fi
-    # https://metallb.universe.tf/configuration/
-    kubectl apply -f srcs/metalib/config.yaml
-}
-
-function check_minikube() {
-    # https://minikube.sigs.k8s.io/docs/start/
-    minikube version
-	if [ "$?" != 0 ]
-	then
-		printf "Minikube isn't installed!\n"
-        brew install minikube
-        sudo mv minikube /usr/local/bin
-        printf "Minikube is installed ✅\n"
-	else
-		printf "Minikube is installed ✅\n"
-	fi
-}
-
-function check_brew() {
-	brew -v
-	# $? is the exit status of the last executed command / true returns 0, false 1
-	if [ "$?" != 0 ]
-	then 
-		printf "Brew isn't installed!\n"
-		read -p "Do you want to install brew? (Y/N): " REP
-		if [[ $REP = "Y" || $REP = "y" || $REP = "" ]]
-		then
-			printf "Installing brew...\n"
-			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-		else
-			printf "Brew is required for this program\n"
-			# exit 1 means exiting with false at the end
-			exit 1
-		fi
-	else
-		printf "Brew is installed ✅\n"
-	fi
-}
+# function metalib_config() {
+#     #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+#     #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+#     kubectl apply -f srcs/metalib/config.yaml
+# }
 
 function main() {
-    check_brew
-    check_minikube
-    metalib_config
-    check_docker
-    start_minikube
+    #start_minikube()
+    minikube start --vm-driver=virtualbox
+    minikube status
+
+    minikube addons enable metrics-server
+    minikube addons enable dashboard
+    minikube addons enable metallb
+
+    minikube kubectl -- get po -A
+    docker-machine rm default
+    docker-machine create --driver virtualbox default
+    eval $(docker-machine env default)
+    eval $(minikube docker-env)
+    kubectl apply -f srcs/metalib/config.yaml
 
     #docker build srcs/nginx -t img-nginx
     #kubectl apply -f srcs/nginx/deployment.yaml
@@ -93,6 +52,7 @@ function main() {
 
     kubectl apply -f srcs/wordpress/pvc.yaml
     kubectl apply -f srcs/wordpress/deployment.yaml
+    kubectl apply -f srcs/wordpress/service.yaml
 
     # https://medium.com/@taweesoft/chapter-1-how-to-easily-deploy-your-web-on-kubernetes-83209a8618be
     # https://blog.gojekengineering.com/diy-set-up-telegraf-influxdb-grafana-on-kubernetes-d55e32f8ce48
@@ -104,6 +64,14 @@ main
 
 ### INFOS
 
+# Debug Docker
+# docker-machine rm default
+# docker-machine create --driver virtualbox default
+# # Used for first run
+# eval $(docker-machine env default)
+# # For testing purpose
+# docker run hello-world
+
 # NGINX 
 # Nginx is a web server that can provide web pages and execute PHP.
 # You need to create a simple Nginx server, it has to be fetchable through Ingres, 
@@ -113,3 +81,28 @@ main
 # This container needs to provide a SSH connection. 
 # SSH is used to access a computer remotly through a shell. 
 # A really simple way to create a SSH server is through the openssh package and then run the sshd daemon.
+
+# METALLB
+# Metallb est un load balancer qui permet aux autres services de type loadbalancer 
+# d'être accessibles depuis l'extérieur du cluster. C'est une 'magouille' qu'on doit 
+# faire pour pouvoir faire tourner le cluster sur une vm et pas dans un cloud avec un 
+# environnement dédié et adapté. 
+# Si tu enlève metallb tu verras que toutes tes external ip resteront en pending
+
+# minikube kubectl -- get po -A
+
+# INFLUXDB
+# InfluxDB is an open-source time series database. Being a time series database, 
+# it suits the intensive workloads of storing and retrieving time-based data 
+# like application metrics, system health metrics (CPU, Memory, Network, Disk) usage etc.
+
+# TELEGRAF
+# Telegraf is an agent for collecting, processing, aggregating, and writing metrics. 
+# It supports multiple outputs, InfluxDB being one of them.
+
+# Grafana
+# Grafana is an open source visualization tool. It is used to create dashboards, 
+# and offers features and plugins to make them dynamic.
+
+# ON VM(linux)
+# https://www.notion.so/Ft_services-VM-852d4f9b0d9a42c1a2de921e4a2ac417
